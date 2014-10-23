f2a16010a69be41980624b70d8971e7c532d7748
poondog <markj338@gmail.com>
2014-10-24 00:14:25 +1100

    BUILD: add basic build script * modified version of Semaphore kernel script * use stock Google GCC 4.7 toolchain * keep it nameless for now
    Signed-off-by: poondog <markj338@gmail.com>

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+BASE_VER=" "
+VER=""
+KERNEL_VER=$BASE_VER$VER
+
+# export LOCALVERSION="."`echo $KERNEL_VER`
+export CROSS_COMPILE=${HOME}/Toolchains/arm-eabi-4.7/bin/arm-eabi-
+
+export ARCH=arm
+export SUBARCH=arm
+export KBUILD_BUILD_USER=poondoge
+
+DATE_START=$(date +"%s")
+
+make "flo_defconfig"
+
+echo "LOCALVERSION="$LOCALVERSION
+echo "CROSS_COMPILE="$CROSS_COMPILE
+echo "ARCH="$ARCH
+
+make -j4
+
+DATE_END=$(date +"%s")
+echo
+DIFF=$(($DATE_END - $DATE_START))
+echo "  Build completed in $(($DIFF / 60)) minutes and $(($DIFF % 60)) seconds."
+echo
