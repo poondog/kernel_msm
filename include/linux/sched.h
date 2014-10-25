4bd2b9029956f7cc47ac2df6af95bf3f9f6894cc
poondog <markj338@gmail.com>
2014-10-25 17:24:49 +1100

    cgroups: fix whitespace errors introduced in previous commits
    Signed-off-by: poondog <markj338@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2840,7 +2840,7 @@ static inline unsigned long rlimit_max(unsigned int limit)
 extern unsigned long task_get_effective_timer_slack(struct task_struct *tsk);
 #else
 static inline unsigned long task_get_effective_timer_slack(
-  		struct task_struct *tsk)
+		struct task_struct *tsk)
 {
 	return tsk->timer_slack_ns;
 }
