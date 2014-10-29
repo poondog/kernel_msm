037d4f92b1fc60032953b07709a69c0d963e23c5
poondog <markj338@gmail.com>
2014-10-30 00:49:43 +1100

    BUILD: initial build script
    Signed-off-by: poondog <markj338@gmail.com>

--- build.sh
+++ build.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-BASE_VER="xx"
+BASE_VER="Semaphore_N4_1.5.0"
 VER=""
 KERNEL_VER=$BASE_VER$VER
 
-# export LOCALVERSION="."`echo $KERNEL_VER`
+export LOCALVERSION="-"`echo $KERNEL_VER`
 export CROSS_COMPILE=${HOME}/Toolchains/arm-eabi-4.7/bin/arm-eabi-
 
 export ARCH=arm
 export SUBARCH=arm
 export KBUILD_BUILD_USER=poondoge
+export KBUILD_BUILD_HOST="semaphore.gr"
 
 DATE_START=$(date +"%s")
 
-make "flo_defconfig"
+make "semaphore_mako_defconfig"
 
 echo "LOCALVERSION="$LOCALVERSION
 echo "CROSS_COMPILE="$CROSS_COMPILE
 echo "ARCH="$ARCH
 
-make -j2
+make -j1
 
 DATE_END=$(date +"%s")
 echo
