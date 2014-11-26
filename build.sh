f247336a8da5546d4533274bc3a9a06d3120e514
poondog <markj338@gmail.com>
2014-11-26 18:51:27 +1100

    BUILD: some cosmetics * add kangaroo ascii * display the build time nicer
    Signed-off-by: poondog <markj338@gmail.com>

--- build.sh
+++ build.sh
@@ -11,31 +11,36 @@ export KBUILD_BUILD_USER=poondoge
 
 DATE_START=$(date +"%s")
 
+echo -e "\e[93m                                                      "
+echo -e "\e[93m        _   __                                        "
+echo -e "\e[93m       | | / /                                        "
+echo -e "\e[93m       | |/ /  __ _ _ __   __ _  __ _ _ __ ___   ___  "
+echo -e "\e[93m       |    \ / _' | '_ \ / _' |/ _' | '__/ _ \ / _ \ "
+echo -e "\e[93m       | |\  \ (_| | | | | (_| | (_| | | | (_) | (_) |"
+echo -e "\e[93m       \_| \_/\__,_|_| |_|\__, |\__,_|_|  \___/ \___/ "
+echo -e "\e[93m                           __/ |                      "
+echo -e "\e[93m                          |___/                       "
+echo -e "\e[93m                     _   __                     _   "
+echo -e "\e[93m                    | | / /                    | |  "
+echo -e "\e[93m                    | |/ /  ___ _ __ _ __   ___| |  "
+echo -e "\e[93m                    |    \ / _ \ '__| '_ \ / _ \ |  "
+echo -e "\e[93m                    | |\  \  __/ |  | | | |  __/ |  "
+echo -e "\e[93m                    \_| \_/\___|_|  |_| |_|\___|_|  "
+echo -e "\e[93m                                                    "
+echo -e "\033[0m                                                   "
+
 make "flo_defconfig"
-
-echo "LOCALVERSION="$LOCALVERSION
-echo "CROSS_COMPILE="$CROSS_COMPILE
-echo "ARCH="$ARCH
-
-echo -e "\e[95m ---------------------------------------------------------------------"
-echo -e "\e[95m                                                      "
-echo -e "\e[97m                         Kangaroo Kernel              "
-echo -e "\e[95m                                                      "
-echo -e "\e[95m                            _,'  ___                  "
-echo -e "\e[95m                          <__\__/   \                 "
-echo -e "\e[95m                             \_  /  _\                "
-echo -e "\e[95m                                \,\ / \\               "
-echo -e "\e[95m                                  //   \\              "
-echo -e "\e[95m                                ,/'     '\_,          "
-echo -e "\e[95m                                                      "
-echo -e "\e[95m                                                      "
-echo -e "\e[95m ---------------------------------------------------------------------"
-echo -e "\033[0m"
 
 make -j2
 
 DATE_END=$(date +"%s")
 echo
 DIFF=$(($DATE_END - $DATE_START))
-echo "  Build completed in $(($DIFF / 60)) minutes and $(($DIFF % 60)) seconds."
+if (( $(($DIFF / 60)) == 0 )); then
+	echo "  Build completed in $(($DIFF % 60)) seconds."
+elif (( $(($DIFF / 60)) == 1 )); then
+	echo "  Build completed in $(($DIFF / 60)) minute and $(($DIFF % 60)) seconds."
+else
+	echo "  Build completed in $(($DIFF / 60)) minutes and $(($DIFF % 60)) seconds."
+fi
 echo
